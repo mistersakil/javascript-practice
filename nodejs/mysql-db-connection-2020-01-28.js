a03dfb01a73da2a4f30b38ed25866bc889c60af3
Sakil Office <mdsakil.com@gmail.com>
2020-01-29 19:41:40 +0600

    node mysql table created

--- nodejs/mysql-db-connection-2020-01-28.js
+++ nodejs/mysql-db-connection-2020-01-28.js
@@ -3,13 +3,20 @@ var mysql = require('mysql')
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: ""
+  password: "",
+  database: "db_node_mysql"
 })
 
 con.connect(err => {
-  if (err) throw err
-  console.log("Connected!")
-  con.query('create database db_node_mysql',(err,data) => {
+  	if (err) throw err
+  	console.log("Connected!")
+	let sql = "create table tbl_users ( "
+			+"id int auto_increment primary key, "
+			+"name varchar(100) not null, "
+			+"email varchar(100) not null, "
+			+"password varchar(100) not null "
+			+")"
+  con.query(sql,(err,data) => {
   	if(err) throw err 
   	console.log(data)
   })
