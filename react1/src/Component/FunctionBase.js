7135e86fb53ee88bce77c2404d890e904b345810
Sakil Mahmud <sakilorb@gmail.com>
2020-03-29 22:42:20 +0600

    js destructuring, rest parameters

---
+++ react1/src/Component/FunctionBase.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+import './Function.css'
+const FunctionBase = ({name}) => {
+	return 	<div className = "my_bg">
+				<h1>Hello - {name}</h1>
+			</div>
+}
+export default FunctionBase
